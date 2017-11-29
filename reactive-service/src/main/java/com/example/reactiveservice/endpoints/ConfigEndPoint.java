fa63f376f509e2d28a8f90112fe41ae75c6b8805
Javier Gomez <Jgomez@cambridgeassociates.com>
2017-11-29 16:23:48 -0600

    routing simplified

--- reactive-service/src/main/java/com/example/reactiveservice/endpoints/ConfigEndPoint.java
+++ reactive-service/src/main/java/com/example/reactiveservice/endpoints/ConfigEndPoint.java
@@ -2,17 +2,23 @@ package com.example.reactiveservice.endpoints;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class ConfigEndPoint {
 
     @Bean
     RouterFunction<?> routes(RouteHandlers routeHandlers){
-        return RouterFunctions.route(RequestPredicates.GET("/react/db/{username}"), routeHandlers::byUserName)
-                .andRoute(RequestPredicates.POST("/react/db/add"), routeHandlers::add);
+        return nest(path("/react/db"),
+                route(GET("/{username}"), routeHandlers::byUserName)
+                .andRoute(POST("/add"), routeHandlers::add));
+
     }
 
 }
