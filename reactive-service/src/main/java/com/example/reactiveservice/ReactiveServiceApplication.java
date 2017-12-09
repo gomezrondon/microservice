4c80eb5eac7f8f70fedebc550e317a80289cdbd3
DESKTOP-8NG6QPL\gomez <gomezrondon@gmail.com>
2017-12-09 10:29:04 -0600

    Fixing proglem with Eureka registry and Netty server.port=0 (does not work)

--- reactive-service/src/main/java/com/example/reactiveservice/ReactiveServiceApplication.java
+++ reactive-service/src/main/java/com/example/reactiveservice/ReactiveServiceApplication.java
@@ -5,13 +5,14 @@ import com.example.reactiveservice.repository.QuoteRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.UUID;
 import java.util.stream.Stream;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReactiveServiceApplication {
 
