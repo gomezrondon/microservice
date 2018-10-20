821c64b12768b8fc8e13f0f957b1a0596f9aeb9b
DESKTOP-8NG6QPL\gomez <gomezrondon@gmail.com>
2018-10-20 17:55:22 -0600

    Removing unnecessary dependency.

--- eureka-service/src/main/java/com/example/stock/eurekaservice/config/Config.java
+++ eureka-service/src/main/java/com/example/stock/eurekaservice/config/Config.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class Config {
 
-    @LoadBalanced  //client side load balancing
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
--- eureka-service/src/main/java/com/example/stock/eurekaservice/service/StockService.java
+++ eureka-service/src/main/java/com/example/stock/eurekaservice/service/StockService.java
@@ -5,20 +5,19 @@ import com.example.stock.eurekaservice.entitie.Stock;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
+
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class StockService {
@@ -26,14 +25,16 @@ public class StockService {
     @Value("${service.db-service.name}")
     private String serviceName;
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     private RestTemplate restTemplate;
     private EurekaClient client;
-    private RestTemplateBuilder restTemplateBuilder;
 
-    public StockService(RestTemplate restTemplate, @Qualifier("eurekaClient") EurekaClient client, RestTemplateBuilder restTemplateBuilder) {
+
+    public StockService(RestTemplate restTemplate, @Qualifier("eurekaClient") EurekaClient client) {
         this.restTemplate = restTemplate;
         this.client = client;
-        this.restTemplateBuilder = restTemplateBuilder;
     }
 
     public List<String> fallback(@PathVariable("username") String username){
@@ -57,30 +58,10 @@ public class StockService {
 
 
     public Stock getGoogleStocks(String quote) {
-
-        String baseUrl = "https://finance.google.com/finance?q=NASDAQ:"+quote+"&output=json";
-        ResponseEntity<String> quotesResponse = restTemplate.exchange(baseUrl, HttpMethod.GET, null,String.class);
-        String data = quotesResponse.getBody();
-
-        try {
-            JSONObject json = new JSONObject(data);
-            String symbol = json.getString("symbol");
-            String name = json.getString("name");
-            String exchange = json.getString("exchange");
-            int id = json.getInt("id");
-
-            JSONArray json2 =   json.getJSONArray("related");
-            String strPrice =json2.getJSONObject(0).getString("l");
-            Double price = Double.valueOf(strPrice.replace(",",""));
-            Stock stock = new Stock(symbol, id, exchange, name, price);
-            return stock;
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-        return null;
+        final int random = new Random().nextInt(1000) + 20;
+        InstanceInfo instanceInfo = client.getNextServerFromEureka(applicationName, false);
+        String port = Integer.toString(instanceInfo.getPort());
+        return new Stock(quote, 0, port, "", random/1.5);
     }
 
 }
